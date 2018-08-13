d27a024952ccbac080ea29d46feb1775e75a68d5
Alex Braun <alexander.g.braun@gmail.com>
2018-08-13 01:56:26 -0700

    added slider and table

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,40 @@
 <template>
     <div id="app">
-        <DevContainer />
+        <!-- <DevContainer /> -->
+        <Table :data="data" :columns="columns"/>
     </div>
 </template>
 
 <script lang="ts">
     import { Component, Vue } from "vue-property-decorator";
     import DevContainer from "./components/DevContainer.vue";
+    import Table from "./components/table.vue";
+    import * as _ from "lodash";
 
-    @Component( {components: { DevContainer } })
+    // @Component( {components: { DevContainer } })
+    // export default class App extends Vue {
+    //     public selected;
+    // }
+    @Component( {components: { Table } })
     export default class App extends Vue {
-        public selected;
+        public rows = [
+            ["scene-001",  "graph-001", "node-001", "func2", 0, 0, "slider", "always"],
+            ["scene-001",  "graph-001", "node-001", "func2", 1, 0, "slider", "always"],
+            ["scene-001",  "graph-001", "node-002", "func3", 2, 0, "slider", "always"],
+            ["scene-002",  "graph-001", "node-001", "func2", 3, 0, "slider", "always"],
+            ["scene-002",  "graph-001", "node-001", "func2", 4, 0, "slider", "always"],
+            ["scene-002",  "graph-001", "node-002", "func3", 5, 0, "slider", "always"],
+        ]
+        public columns = [
+            "scene-id", "graph-id", "node-id",
+            "node-name", "value", "default", "widget", "pattern"
+        ];
+        public data = _.map(this.rows, (item) => (_.zipObject(this.columns, item)) );
     }
 </script>
 
 <style lang="less">
-// body {
-//     background: #242424;
-// }
-
-#app {
-    font-family: 'Avenir', Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
-    margin-top: 60px;
+body {
+    background: #242424;
 }
 </style>
---
+++ src/components/slider.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="widget-container">
+        <vue-slider ref="slider" v-model="value">
+        </vue-slider>
+    </div>
+</template>
+
+<script lang="ts">
+    import Vue from "vue";
+    import { Component, Prop } from "vue-property-decorator";
+    import vueSlider from "vue-slider-component";
+
+    @Component( {components: { vueSlider }} )
+    export default class Slider extends Vue {
+        @Prop({default: 0})
+        public value: number;
+    }
+</script>
+
+<style scoped lang="less">
+</style>
