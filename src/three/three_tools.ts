7b59a77dae2fe9b53138a7b95e7f0e760c8d174f
Alex Braun <alexander.g.braun@gmail.com>
2018-11-05 20:59:59 -0800

    fixed color

--- src/three/three_tools.ts
+++ src/three/three_tools.ts
@@ -1,4 +1,5 @@
 import * as _ from "lodash";
+import * as tiny_color from "tinycolor2";
 // -----------------------------------------------------------------------------
 
 export const TRANSLATE_KEYS: string[] = [
@@ -140,135 +141,20 @@ export interface IHSV {
     v: number;
 }
 
-/**
- * Converts an RGB color value to HSV. Conversion formula
- * adapted from http://en.wikipedia.org/wiki/HSV_color_space.
- * Assumes hsv is in the 0...1 space
- * returns rgb is in the 0...255 space
- *
- * @param IRGB object with r,g,b keys
- * @return IHSV object with h,s,v keys
- */
 export function rgb_to_hsv(rgb: IRGB): IHSV {
-    const r: number = rgb.r / 255;
-    const g: number = rgb.g / 255;
-    const b: number = rgb.b / 255;
-
-    const max = Math.max(r, g, b);
-    const min = Math.min(r, g, b);
-    const d = max - min;
-
-    // hue
-    let h: number;
-    if (max === min) {
-        h = 0;
-    }
-    else if (max === r) {
-        h = 60 * (0 + ((g - b) / d));
-    }
-    else if (max === g) {
-        h = 60 * (2 + ((b - r) / d));
-    }
-    else if (max === b) {
-        h = 60 * (4 + ((r - g) / d));
-    }
-    if (h < 0) {
-        h += 360;
-    }
-    h /= 360;
-
-    // saturation
-    let s: number;
-    if (max === 0) {
-        s = 0;
-    }
-    else {
-        s = d / max;
-    }
-
-    // value
-    const v = max;
-
-    return {
-        h: h,
-        s: s,
-        v: v,
+    const color = {
+        r: rgb.r * 255,
+        g: rgb.g * 255,
+        b: rgb.b * 255,
     };
+    return tiny_color.fromRatio(color).toHsv();
 }
 
-/**
- * Converts an HSV color value to RGB. Conversion formula
- * adapted from http://en.wikipedia.org/wiki/HSV_color_space.
- * Assumes hsv is in the 0...1 space
- * returns rgb is in the 0...255 space
- *
- * @param IHSV object with h,s,v keys
- * @return IRGB object with r,g,b keys
- */
 export function hsv_to_rgb(hsv: IHSV): IRGB {
-    const h: number = hsv.h;
-    const s: number = hsv.s;
-    const v: number = hsv.v;
-
-    const hp: number = h * 360 / 60;
-
-    const c = v * s;
-    const x = c * (1 - Math.abs((hp % 2) - 1));
-
-    let rgb: number[] = [];
-
-    if (h === undefined) {
-        rgb = [0, 0, 0];
-    }
-    else if (0 <= hp && hp <= 1) {
-        rgb = [c, x, 0];
-    }
-    else if (1 <= hp && hp <= 2) {
-        rgb = [x, c, 0];
-    }
-    else if (2 <= hp && hp <= 3) {
-        rgb = [0, c, x];
-    }
-    else if (3 <= hp && hp <= 4) {
-        rgb = [0, x, c];
-    }
-    else if (4 <= hp && hp <= 5) {
-        rgb = [x, 0, c];
-    }
-    else if (5 <= hp && hp <= 6) {
-        rgb = [c, 0, x];
-    }
-
-    const m: number = v - c;
-    rgb = rgb.map(x => x + m);
-    rgb = rgb.map(x => x * 255);
+    const color = tiny_color.fromRatio(hsv).toRgb();
     return {
-        r: rgb[0],
-        g: rgb[1],
-        b: rgb[2],
+        r: color.r / 255,
+        g: color.g / 255,
+        b: color.b / 255,
     };
-    // const i = Math.floor(h * 6);
-    // const f = h * 6 - i;
-    // const p = v * (1 - s);
-    // const q = v * (1 - f * s);
-    // const t = v * (1 - (1 - f) * s);
-
-    // switch (i % 6) {
-    //     case 0: r = v, g = t, b = p; break;
-    //     case 1: r = q, g = v, b = p; break;
-    //     case 2: r = p, g = v, b = t; break;
-    //     case 3: r = p, g = q, b = v; break;
-    //     case 4: r = t, g = p, b = v; break;
-    //     case 5: r = v, g = p, b = q; break;
-    // }
-
-    // r *= 255;
-    // g *= 255;
-    // b *= 255;
-
-    // return {
-    //     r: r,
-    //     g: g,
-    //     b: b,
-    // };
 }
