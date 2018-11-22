6b1cb53372fcb429458943ed7b8a3810c51e11e8
Alex Braun <alexander.g.braun@gmail.com>
2018-11-21 23:09:28 -0700

    improved test_scene definition

--- test/test_scene.ts
+++ test/test_scene.ts
@@ -1,11 +1,20 @@
+import * as _ from "lodash";
 import * as tools from "../src/core/tools";
 // -----------------------------------------------------------------------------
 
 const cyan2 = tools.HSV_COLORS["aspect_cyan_2"];
 const grey2 = tools.HSV_COLORS["aspect_grey_2"];
 
+const sid0: string  = "uuid0";
+const gid0: string  = "uuid1";
+const eid0: string  = "uuid2";
+const eid1: string  = "uuid3";
+const nid0: string  = "uuid4";
+const ipid0: string = "uuid5";
+const opid0: string = "uuid6";
+
 const proto_scene: object = {
-    "id":              "uuid0",
+    "id":              sid0,
     "resource_<id>":   "",
     "display/name":    "",
     "display/info":    "",
@@ -13,22 +22,22 @@ const proto_scene: object = {
 };
 
 const proto_edge: object = {
-    "id":                       "uuid1",
-    "from":                     "",
-    "to":                       "uuid4",
+    "id":                       eid0,
+    "source":                   "",
+    "destination":              ipid0,
     "type":                     "",
     "display/name":             "edge",
     "display/info":             "",
     "display/order":            "",
     "display/selected":         "",
     "display/visible":          true,
-    // "display/from/translate/x": 0,
-    // "display/from/translate/y": 4,
-    // "display/from/translate/z": 0,
-    "display/from/visible":     true,
-    // "display/to/translate/x":   0,
-    // "display/to/translate/y":   0,
-    // "display/to/translate/z":   0,
+    // "display/source/translate/x":      0,
+    // "display/source/translate/y":      4,
+    // "display/source/translate/z":      0,
+    "display/source/visible":     true,
+    // "display/destination/translate/x": 0,
+    // "display/destination/translate/y": 0,
+    // "display/destination/translate/z": 0,
     "display/to/visible":       false,
     "display/scale/x":          1,
     "display/scale/y":          1,
@@ -41,7 +50,7 @@ const proto_edge: object = {
 };
 
 const proto_graph: object = {
-    "id":                  "uuid2",
+    "id":                  gid0,
     "type":                "",
     "innode":              "",
     "outnode":             "",
@@ -62,8 +71,11 @@ const proto_graph: object = {
     "display/scale/z":     1,
 };
 
+const edge2 = _.clone(proto_edge);
+edge2["id"] = eid1;
+
 const proto_node: object = {
-    "id":                            "uuid3",
+    "id":                            nid0,
     "type":                          "",
     "module":                        "",
     "function":                      "",
@@ -95,31 +107,73 @@ const proto_node: object = {
 };
 
 const proto_inport: object = {
-    "id":                       "uuid4",
-    "value":                    "",
-    "default_value":            "",
-    "type":                     "",
-    "lock":                     "",
-    "display/name":             "inport",
-    "display/widget":           "",
-    "display/options":          "",
-    "display/info":             "",
-    "display/order":            "",
+    "id":                            ipid0,
+    "value":                         "",
+    "default_value":                 "",
+    "type":                          "",
+    "lock":                          "",
+    "display/name":                  "inport",
+    "display/widget":                "",
+    "display/options":               "",
+    "display/info":                  "",
+    "display/order":                 "",
+    "display/selected":              "",
+    "display/visible":               true,
+    "display/translate/x":           0,
+    "display/translate/y":           0,
+    "display/translate/z":           0,
+    "display/scale/x":               1,
+    "display/scale/y":               1,
+    "display/scale/z":               1,
+    "display/color/hue":             cyan2.h,
+    "display/color/saturation":      cyan2.s,
+    "display/color/value":           cyan2.v,
+    "display/color/alpha":           cyan2.a,
+    "display/font/color/hue":        grey2.h,
+    "display/font/color/saturation": grey2.s,
+    "display/font/color/value":      grey2.v,
+    "display/font/color/alpha":      grey2.a,
+    "display/font/text":             "DEFAULT TEXT",
+    "display/font/family":           "mono",
+    "display/font/style":            "normal",
+    "display/font/size":             300,
 };
 
 const proto_outport: object = {
-    "id":                       "uuid5",
-    "display/name":             "outport",
-    "display/info":             "",
-    "display/order":            "",
+    "id":                            opid0,
+    "display/name":                  "outport",
+    "display/widget":                "",
+    "display/options":               "",
+    "display/info":                  "",
+    "display/order":                 "",
+    "display/selected":              "",
+    "display/visible":               true,
+    "display/translate/x":           0,
+    "display/translate/y":           0,
+    "display/translate/z":           0,
+    "display/scale/x":               1,
+    "display/scale/y":               1,
+    "display/scale/z":               1,
+    "display/color/hue":             cyan2.h,
+    "display/color/saturation":      cyan2.s,
+    "display/color/value":           cyan2.v,
+    "display/color/alpha":           cyan2.a,
+    "display/font/color/hue":        grey2.h,
+    "display/font/color/saturation": grey2.s,
+    "display/font/color/value":      grey2.v,
+    "display/font/color/alpha":      grey2.a,
+    "display/font/text":             "DEFAULT TEXT",
+    "display/font/family":           "mono",
+    "display/font/style":            "normal",
+    "display/font/size":             300,
 };
 
 const temp: object = {};
-temp["scene_<id>"] = proto_scene;
-temp["scene_<id>"]["edge_<id>"] = proto_edge;
-temp["scene_<id>"]["graph_<id>"] = proto_graph;
-temp["scene_<id>"]["graph_<id>"]["edge_<id>"] = proto_edge;
-temp["scene_<id>"]["graph_<id>"]["node_<id>"] = proto_node;
-temp["scene_<id>"]["graph_<id>"]["node_<id>"]["inport_<id>"] = proto_inport;
-temp["scene_<id>"]["graph_<id>"]["node_<id>"]["outport_<id>"] = proto_outport;
+temp[`scene_${sid0}`] = proto_scene;
+temp[`scene_${sid0}`][`edge_${eid0}`] = proto_edge;
+temp[`scene_${sid0}`][`graph_${gid0}`] = proto_graph;
+temp[`scene_${sid0}`][`graph_${gid0}`][`edge_${eid1}`] = edge2;
+temp[`scene_${sid0}`][`graph_${gid0}`][`node_${nid0}`] = proto_node;
+temp[`scene_${sid0}`][`graph_${gid0}`][`node_${nid0}`][`inport_${ipid0}`] = proto_inport;
+temp[`scene_${sid0}`][`graph_${gid0}`][`node_${nid0}`][`outport_${opid0}`] = proto_outport;
 export const scene: object = tools.flatten(temp);
