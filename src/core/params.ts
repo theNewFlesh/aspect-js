9173b25bf251f0d79088e1ba462a056559226d93
Alex Braun <alexander.g.braun@gmail.com>
2018-11-21 23:45:42 -0700

    added methods to params

--- src/core/params.ts
+++ src/core/params.ts
@@ -36,54 +36,95 @@ export class Params {
         return new Params(data);
     }
 
-    public get_full_graph(id: string): Params {
+    public get_full_graph(graph_id: string): Params {
         const data: object = this.__data
-            .filter(x => x.match("graph_" + id + "\/"), "key")
+            .filter(x => x.match("graph_" + graph_id + "\/"), "key")
             .to_object();
         return new Params(data);
     }
 
-    public get_full_node(id: string): Params {
+    public get_full_node(node_id: string): Params {
         const data: object = this.__data
-            .filter(x => x.match("node_" + id + "\/"), "key")
+            .filter(x => x.match("node_" + node_id + "\/"), "key")
             .to_object();
         return new Params(data);
     }
 
-    public get_graph(id: string): Params {
+    public get_graph(graph_id: string): Params {
         const data: object = this.__data
-            .filter(x => x.match("graph_" + id + "\/"), "key")
+            .filter(x => x.match("graph_" + graph_id + "\/"), "key")
             .filter(x => !x.match("node|edge|inport|outport"), "key")
             .to_object();
         return new Params(data).strip_id();
     }
 
-    public get_node(id: string): Params {
+    public get_node(node_id: string): Params {
         const data: object = this.__data
-            .filter(x => x.match("node_" + id + "\/"), "key")
+            .filter(x => x.match("node_" + node_id + "\/"), "key")
             .filter(x => !x.match("inport|outport"), "key")
             .to_object();
         return new Params(data).strip_id();
     }
 
-    public get_edge(id: string): Params {
+    public get_edge(edge_id: string): Params {
         const data: object = this.__data
-            .filter(x => x.match("edge_" + id + "\/"), "key")
+            .filter(x => x.match("edge_" + edge_id + "\/"), "key")
             .to_object();
         return new Params(data).strip_id();
     }
 
-    public get_inport(id: string): Params {
+    public get_inport(inport_id: string): Params {
         const data: object = this.__data
-            .filter(x => x.match("inport_" + id + "\/"), "key")
+            .filter(x => x.match("inport_" + inport_id + "\/"), "key")
             .to_object();
         return new Params(data).strip_id();
     }
 
-    public get_outport(id: string): Params {
+    public get_outport(outport_id: string): Params {
         const data: object = this.__data
-            .filter(x => x.match("outport_" + id + "\/"), "key")
+            .filter(x => x.match("outport_" + outport_id + "\/"), "key")
             .to_object();
         return new Params(data).strip_id();
+    }
+
+    public to_graph(graph_id: string): object {
+        return this.get_graph(graph_id).to_object();
+    }
+
+    public to_node(node_id: string): object {
+        return this.get_node(node_id).to_object();
+    }
+
+    public to_edge(edge_id: string): object {
+        return this.get_edge(edge_id).to_object();
+    }
+
+    public to_inport(inport_id: string): object {
+        return this.get_inport(inport_id).to_object();
+    }
+
+    public to_outport(outport_id: string): object {
+        return this.get_outport(outport_id).to_object();
+    }
+
+    public _to_ports(node_id: string, port_type: string): object[] {
+        const data: object[] = this.get_full_node(node_id).__data
+            .filter(x => x.match(port_type + "_.*?\/"), "key")
+            .assign(x => x.key.match("(" + port_type + "_.*?)\/")[1], "id")
+            .group_by(x => x.to_object(), "id")
+            .to_array();
+        const output: object[] = [];
+        for (const port of data) {
+            output.push( new Params(port).strip_id().to_object() );
+        }
+        return output;
+    }
+
+    public to_inports(node_id: string): object[] {
+        return this._to_ports(node_id, "inport");
+    }
+
+    public to_outports(node_id: string): object[] {
+        return this._to_ports(node_id, "outport");
     }
 }
