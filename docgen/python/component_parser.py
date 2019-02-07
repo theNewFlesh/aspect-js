239ed597332763ebaba7162a7a6c19765f46bbda
Alex Braun <alexander.g.braun@gmail.com>
2019-02-06 19:22:30 -0800

    completed component_parser

--- docgen/python/component_parser.py
+++ docgen/python/component_parser.py
@@ -4,24 +4,20 @@ import pandas as pd
 from pandas import DataFrame
 from pyparsing import Group, Or, Regex, delimitedList, Optional, Suppress, ZeroOrMore
 # ------------------------------------------------------------------------------
+
 def try_(func, item):
     try:
         return func(item)
     except:
         return ''
-
-def get_files(base):
-    files = os.listdir(base)
-    files = filter(lambda x: re.search('\.vue$', x), files)
-    files = [os.path.join(base, x) for x in files]
-    return files
+# ------------------------------------------------------------------------------
 
 def component_to_dataframe(fullpath):
     with open(fullpath) as f:
         data = f.readlines()
     data = DataFrame(data, columns=['text'])
     data.text = data.text.apply(lambda x: x.strip('\n'))
-    data['content'] = data.text.apply(parse_component)
+    data['content'] = data.text.apply(parse_line)
     data = data[data.content != {}]
     
     start = data.content\
@@ -36,6 +32,53 @@ def component_to_dataframe(fullpath):
     data['content_type'] = data.content.apply(lambda x: x['content_type'])
     data['content'] = data.content.apply(lambda x: x['content'])
 
+    return data
+
+def _merge_content_types(row):
+    row.content['content_type'] = row.content_type
+    return row.content
+
+def _merge_content(items):
+    temp = {}
+    for item in items:
+        key = item['content_type']
+        if not key in temp.keys():
+            temp[key] = []
+            
+        tempitem = {}
+        for k in filter(lambda x: x != 'content_type', item.keys()):
+            tempitem[k] = item[k]
+        temp[key].append(tempitem)
+    
+    output = {}
+    for k, v in temp.items():
+        if k == 'docline':
+            output['params'] = list(filter(lambda x: 'param' in x.keys(), v))
+            
+            returns = list(filter(lambda x: 'returns' in x.keys(), v))
+            output['returns'] = None
+            if len(returns) > 0:
+                output['returns'] = returns[0]
+                
+            extra = filter(lambda x: 'param' not in x.keys(), v)
+            extra = filter(lambda x: 'returns' not in x.keys(), extra)
+            desc = filter(lambda x: 'description' in x.keys(), extra)
+            desc = [x['description'] for x in desc]
+            desc = '\n'.join(desc)
+            output['description'] = desc
+            
+            extra = filter(lambda x: 'description' not in x.keys(), extra)
+            output['extra'] = list(extra)
+            
+        elif k == 'method':
+            output[k] = v[0]
+            
+        else:
+            output[k] = v
+        
+    return output
+
+def dataframe_to_list(data):
     # assign docstring identifiers
     mask = data.content_type.apply(lambda x: x in ['docstart', 'docstop'])
     temp = data[mask].index.tolist()
@@ -49,10 +92,27 @@ def component_to_dataframe(fullpath):
     data.id.ffill(inplace=True)
     data.tail(1).id = pd.np.nan
 
+    ignore = [
+        'script_start',
+        'script_stop',
+        'docstart',
+        'docstop'
+    ]
+    mask = data.content_type.apply(lambda x: x not in ignore)
+
+    # group by contiguos docstring and signature blocks
+    data.content = data.apply(_merge_content_types, axis=1)
+    data.dropna(inplace=True)
+    data = data[mask].groupby('id')\
+        .agg(lambda x: [x.content.tolist()])\
+        .content.apply(_merge_content)
+    data.reset_index(inplace=True, drop=True)
+    data = data.tolist()
+
     return data
 # ------------------------------------------------------------------------------
 
-def parse_component(line):
+def parse_line(line):
     # generic terms
     _name_re = '[a-zA-Z_][a-zA-Z0-9_]*'
     name_re  = Regex(_name_re)
@@ -148,9 +208,34 @@ def parse_component(line):
     return result
 # ------------------------------------------------------------------------------
 
+def get_files(base):
+    files = os.listdir(base)
+    files = filter(lambda x: re.search('\.vue$', x), files)
+    files = [os.path.join(base, x) for x in files]
+    return files
+
+def parse_component(fullpath):
+    data = component_to_dataframe(fullpath)
+    data = dataframe_to_list(data)
+    return data
+
+def parse_components(base):
+    files = get_files(base)
+    output = []
+    for f in files:
+        output.append(dict(
+            content=parse_component(f),
+            fullpath=f,
+            name=os.path.splitext(os.path.split(f)[-1])[0]
+        ))
+    return output
+# ------------------------------------------------------------------------------
+
 __all__ = [
-    'try_',
     'get_files',
     'component_to_dataframe',
-    'parse_component'
+    'dataframe_to_list',
+    'parse_line',
+    'parse_component',
+    'parse_components'
 ]
