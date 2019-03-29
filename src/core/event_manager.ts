d657e406288dd4038eeffa6ca45d04af99d71a8e
Alex Braun <alexander.g.braun@gmail.com>
2019-03-28 19:13:02 -0700

    added EventManager docs

--- src/core/event_manager.ts
+++ src/core/event_manager.ts
@@ -4,11 +4,17 @@ import * as _ from "lodash";
 import * as Mousetrap from "mousetrap";
 // -----------------------------------------------------------------------------
 
+/**
+ * Subevent interface for internal use of EventManager
+ */
 export interface ISubEvent {
     name: string;
     value: any;
 }
 
+/**
+ * Event interface for decomoposing event names into usefull context fields
+ */
 export interface IEvent {
     name: string;
     context: string;
@@ -20,6 +26,14 @@ export interface IEvent {
     value: any;
 }
 
+/**
+ * Converts an event name into IEvent object
+ * @param name Name of event
+ * @param value Value of event
+ * @param row NodePane row object
+ * @param column NodePane column object
+ * @returns IEvent object
+ */
 export function to_event(name: string, value: any, row?: object, column?: string): IEvent {
     const fields: string[] = name.split("-");
     const output: IEvent = {
@@ -40,6 +54,16 @@ export function to_event(name: string, value: any, row?: object, column?: string
     return output;
 }
 
+/**
+ * Class for managing all aspect-js events.
+ * 
+ * EventManager receives a config object which map user keyboard and mouse
+ * events to event handlers. EventManages also has maintains state on various
+ * modes of interaction:
+ * - gui_mode: mouse position over GUI areas
+ * - interaction_mode: types of selection such as area and deselect
+ * - selection_mode: for selecting different DAG components
+ */
 export class EventManager {
     constructor(config: object) {
         this.config = config;
@@ -66,11 +90,39 @@ export class EventManager {
         EventBus.$on("node_pane-cell-inport-update", this.on_node_pane_cell_inport_update);
     }
 
+    /**
+     * Keyboard/mouse event -> event handler configuration
+     */
     public config: object;
-    public _gui_mode: string = "dag_pane"; // node_pane, search_pane
-    public _interaction_mode: string = "highlight"; // select, deselect, select_area, deselect_area
-    public _selection_mode: string = "any"; // graph, node, edge
+
+    /**
+     * Mode representing what main GUI element a user's mouse if hovering over
+     * Options include: dag_pane, node_pane and search_pane
+     */
+    public _gui_mode: string = "dag_pane";
+
+    /**
+     * Mode representation of a users intended interations with a DAG component
+     * Options include: highlight, select, deselect, select_area and deselect_area
+     */
+    public _interaction_mode: string = "highlight";
+
+    /**
+     * Mode for selecting different DAG components
+     * Options include: any, graph, node and edge
+     */
+    public _selection_mode: string = "any";
+
+    /**
+     * Vertical pane separator position (separating DagPane from NodePane) as a
+     * percentage of app width from left to right
+     */
     public _vertical_pane_separator: number = 75;
+
+    /**
+     * Pair of IVector3 objects representing a 3D rectilnear prism of DAG
+     * component selection
+     */
     public _selection_area: IVector3[] = [];
 
     /**
@@ -78,10 +130,19 @@ export class EventManager {
      */
     public _coordinates: IVector3 = { x: 0, y: 0, z: 0 };
 
+    /**
+     * Event handler for changes to the vertical pane separator
+     * @param width percentage of app width from left to right
+     */
     public on_vertical_pane_separator_update(width: number): void {
         this._vertical_pane_separator = width;
     }
 
+    /**
+     * Converts IEvent object to EventManager event handler method
+     * @param event IEvent object
+     * @return Referebce to EventManager method
+     */
     public _lookup_handler(event: IEvent): string {
         let key: string = [
             "on",
@@ -94,6 +155,11 @@ export class EventManager {
         return this[key];
     }
 
+    /**
+     * Generic event handler method which looks up an event handler using with
+     * a given event and then calls that handler with the event
+     * @param event IEvent to be used
+     */
     public on_event(event: IEvent): void {
         const handler: any = this._lookup_handler(event);
         handler(event);
